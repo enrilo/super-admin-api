6ed184770c19512ff6b30d39c78c14d648bc6f1c
shoaib.shaikh <shoaib.shaikh@rapidops.com>
2025-12-15 15:19:18 +0530

    Updates in our company

--- src/models/OurCompany.js
+++ src/models/OurCompany.js
@@ -4,12 +4,13 @@ const ourCompanySchema = new mongoose.Schema(
     {
         logo_url: { type: String, default:"https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg", },
         name: { type: String, required: true },
+        address: { type: String, required: true },
         website: { type: String, required: true },
-        gst_number: { type: String, required: true },
-        linkedin: { type: String, required: true },
-        twitter: { type: String, required: true },
-        facebook: { type: String, required: true },
-        instagram: { type: String, required: true },
+        gst_number: { type: String, required: false },
+        linkedin: { type: String, required: false },
+        twitter: { type: String, required: false },
+        facebook: { type: String, required: false },
+        instagram: { type: String, required: false },
     },
     { timestamps: true }
 );
